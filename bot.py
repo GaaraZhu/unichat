3f34c3a81e916ac4ad095a47b8571af5c24c6466
GaryZhu <Gary@movio.co>
2016-07-07 18:31:15 +1200

    m3: add translation for text messages this fixes #6

--- bot.py
+++ bot.py
@@ -2,16 +2,18 @@ import logging
 import time
 import sys
 
+from GoogleApi import Translator
 from slack import UniChatSlackClient
 from itchat.client import client as WeChatClient
 
 
 class Bot(object):
-    def __init__(self, token, channelName):
+    def __init__(self, token, channelName, googleApikey):
         self.channelName = channelName
-        self.wechatClient = WeChatClient()
         self.slackClient = UniChatSlackClient(token)
         self.wechatGroup = None
+        self.wechatClient = WeChatClient()
+        self.translator = Translator(googleApikey)
 
     def bot_main(self):
         self.channel = self.slackClient.attach_channel(self.channelName)
@@ -44,20 +46,24 @@ class Bot(object):
                 self.wechatGroup = msg['FromUserName']
             print("Sending message to slack: %s" % msg['Content'])
             self.channel.send_message(msg['ActualNickName'] + ": " + msg['Content'])
+            translatedMsg = self.translator.toEnglish(msg['Content'])
+            self.channel.send_message("[Translation]: %s : %s" % (msg['ActualNickName'], translatedMsg))
 
     def process_slack_messages(self, msgs):
         for msg in msgs:
             if self.wechatGroup:
                 print("Sending message to wechat: %s" % msg[u'text'])
+                translatedMsg = self.translator.toChinese(msg[u'text'])
+                self.wechatClient.send_msg("[Translation]: %s" % translatedMsg, self.wechatGroup)
                 self.wechatClient.send_msg(msg[u'text'], self.wechatGroup)
             else:
                 print("No WeChat group")
 
-
 def main():
     token = sys.argv[1]
     channel = sys.argv[2]
-    bot = Bot(token, channel)
+    googleApikey = sys.argv[3]
+    bot = Bot(token, channel, googleApikey)
     print("Starting bot...")
     bot.bot_main()
 
